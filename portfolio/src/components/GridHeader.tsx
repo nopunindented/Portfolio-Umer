ea20af8ed16b6bddb73245c019f08ced3629d2ff
umerindented <umerfiaz251@gmail.com>
2024-02-22 23:49:56 -0700

    "navbar/Very good grid setup. Need to add media query and hamburger menu"

--- portfolio/src/components/GridHeader.tsx
+++ portfolio/src/components/GridHeader.tsx
@@ -8,6 +8,7 @@ const GridHeader: React.FC = () => {
             <div className="name_navbar">Muhammad Umer Fiaz</div>
             <div className="work_navbar">Work Experience</div>
             <div className="projects_navbar">Projects</div>
+            <div className="extra_navbar">Extracurriculars</div>
       </div>
     );
   };
