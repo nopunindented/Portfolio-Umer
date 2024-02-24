ec012b89796c19f80544028e462da5b407389fae
umerindented <umerfiaz251@gmail.com>
2024-02-24 00:14:16 -0700

    "hamburger_menu/Switched to hamburger-react bcz blue transition is only inspect-element"

--- portfolio/src/components/GridHeader.tsx
+++ portfolio/src/components/GridHeader.tsx
@@ -1,36 +1,35 @@
 import React from "react";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import "./header.css";
 import HamMenu from "./HamMenu";
-
-interface ScreenSizeState {
-  width: number;
-}
+import Hamburger from "hamburger-react";
 
 interface ScreenSizeState {
   width: number;
 }
 
 const GridHeader: React.FC = () => {
-
-  const [screenSize, setScreenSize] = useState<ScreenSizeState>({ width: window.innerWidth });
+  const [screenSize, setScreenSize] = useState<ScreenSizeState>({
+    width: window.innerWidth,
+  });
+  const [isOpen, setOpen] = useState<boolean>(false);
 
   useEffect(() => {
     const handleResize = () => {
-      setScreenSize(
-        {
-          width: window.innerWidth
-        });
-    }
+      setScreenSize({
+        width: window.innerWidth,
+      });
+    };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
-  
+
   return (
+    
     <div className="wrapper">
       <div className="name_navbar">Muhammad Umer Fiaz</div>
       {screenSize.width > 550 ? (
@@ -40,10 +39,10 @@ const GridHeader: React.FC = () => {
           <div className="extra_navbar">Extracurriculars</div>
         </>
       ) : (
-        <HamMenu />
+        <Hamburger color="white" size={24} toggled={isOpen} toggle={setOpen}/>
       )}
     </div>
   );
-      }
-  
-  export default GridHeader;
+};
+
+export default GridHeader;
