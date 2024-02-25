8ae2e9883dd3be9785811d9f172859fb77c7fa82
umerindented <umerfiaz251@gmail.com>
2024-02-24 23:21:42 -0700

    "hamburger-menu/Added great positioning for hamburger menu"

--- portfolio/src/components/HamMenu.tsx
+++ portfolio/src/components/HamMenu.tsx
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
-
+import Hamburger from "hamburger-react";
 
 const HamMenu: React.FC = () => {
   const [isOpen, setOpen] = useState<boolean>(false);
 
   return (
-    <div className="menu-btn">
-      <div className="menu-btn_burger" />
-    </div>
+    <Hamburger color="white" size={16} toggled={isOpen} toggle={setOpen} />
   );
 };
 
