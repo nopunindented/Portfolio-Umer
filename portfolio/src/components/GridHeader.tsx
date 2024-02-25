9b2436d335ce2fbb5c06017c760c9f3702937209
Muhammad Umer Fiaz <umerfiaz251@gmail.com>
2024-02-24 23:23:27 -0700

    "hamburger-menu/Added great positioning for hamburger menu" (#8)

--- portfolio/src/components/GridHeader.tsx
+++ portfolio/src/components/GridHeader.tsx
@@ -9,10 +9,10 @@ interface ScreenSizeState {
 }
 
 const GridHeader: React.FC = () => {
+  const [isOpen, setOpen] = useState<boolean>(false);
   const [screenSize, setScreenSize] = useState<ScreenSizeState>({
     width: window.innerWidth,
   });
-  const [isOpen, setOpen] = useState<boolean>(false);
 
   useEffect(() => {
     const handleResize = () => {
@@ -29,7 +29,6 @@ const GridHeader: React.FC = () => {
   }, []);
 
   return (
-    
     <div className="wrapper">
       <div className="name_navbar">Muhammad Umer Fiaz</div>
       {screenSize.width > 550 ? (
@@ -39,7 +38,17 @@ const GridHeader: React.FC = () => {
           <div className="extra_navbar">Extracurriculars</div>
         </>
       ) : (
-        <Hamburger color="white" size={24} toggled={isOpen} toggle={setOpen}/>
+        <div
+          className="hamburger-position"
+          style={{ position: "absolute", top: "-16%", right: "0%" }}
+        >
+          <Hamburger
+            color="white"
+            size={16}
+            toggled={isOpen}
+            toggle={setOpen}
+          />
+        </div>
       )}
     </div>
   );
