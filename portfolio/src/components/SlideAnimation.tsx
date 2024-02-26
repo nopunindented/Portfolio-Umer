fd6147981abd45b6d7ed1f1a22215eda5f919d95
umerindented <umerfiaz251@gmail.com>
2024-02-25 22:42:00 -0700

    "slides-animation/Big breakthrough on bg distortion prevention"

--- portfolio/src/components/SlideAnimation.tsx
+++ portfolio/src/components/SlideAnimation.tsx
@@ -5,6 +5,9 @@ const SlideAnimation: React.FC = () => {
   return (
     <>
       <div className="bg">
+        <div className="typewriter-container">
+          <TypingIntro />
+        </div>
       </div>
     </>
   );
