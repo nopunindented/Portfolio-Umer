8b61cce780d9fdd4032ff97265606868ae834b33
nopunindented <umerfiaz251@gmail.com>
2024-02-26 20:59:06 -0700

    "Slightly fixed descript and styling"

--- portfolio/src/components/DescriptIntro.tsx
+++ portfolio/src/components/DescriptIntro.tsx
@@ -8,7 +8,7 @@ const DescriptIntro: React.FC = () => {
         I'm a 3rd year undergraduate software engineering student at the University of Alberta, a Software Developer Intern at
         SUBNET Solutions, and an incoming Software Engineer Intern at Dayforce, on the Developer Experience Team (Infrastructure/DevOps & Internal Software Tool Dev).
         
-        I enjoy creating applications that tackle social problems, help university students & clubs, or are just cool in general :)
+        I enjoy creating applications that tackle social problems (or are just cool) and I love being part of university clubs!:)
         </div>
       </div>
     </>
