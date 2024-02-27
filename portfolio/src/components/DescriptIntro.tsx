052be01509c58145695513e50f0267ec8d34dbde
nopunindented <umerfiaz251@gmail.com>
2024-02-26 20:55:40 -0700

    "Done for today. Need media queries for descript and name intro overlap prevent"

--- portfolio/src/components/DescriptIntro.tsx
+++ portfolio/src/components/DescriptIntro.tsx
@@ -5,7 +5,9 @@ const DescriptIntro: React.FC = () => {
     <>
       <div className="descript_intro">
         <div>
-        I'm a 3rd year undergraduate software engineering student at the University of Alberta.
+        I'm a 3rd year undergraduate software engineering student at the University of Alberta, a Software Developer Intern at
+        SUBNET Solutions, and an incoming Software Engineer Intern at Dayforce, on the Developer Experience Team (Infrastructure/DevOps & Internal Software Tool Dev).
+        
         I enjoy creating applications that tackle social problems, help university students & clubs, or are just cool in general :)
         </div>
       </div>
