9e64f2421193ea71de8519a7a48f63bf2f74c12a
nopunindented <umerfiaz251@gmail.com>
2024-02-26 20:50:43 -0700

    "Intro page essentially done for text-related elements"

---
+++ portfolio/src/components/DescriptIntro.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const DescriptIntro: React.FC = () => {
+  return (
+    <>
+      <div className="descript_intro">
+        <div>
+        I'm a 3rd year undergraduate software engineering student at the University of Alberta.
+        I enjoy creating applications that tackle social problems, help university students & clubs, or are just cool in general :)
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DescriptIntro;
--- portfolio/src/components/SlideAnimation.tsx
+++ portfolio/src/components/SlideAnimation.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import TypingIntro from "./TypingIntro";
 import NameIntro from "./NameIntro";
+import DescriptIntro from "./DescriptIntro";
 
 const SlideAnimation: React.FC = () => {
   return (
     <>
       <div className="bg">
+      <DescriptIntro />
         <NameIntro />
         <div className="typewriter-container">
           <TypingIntro />
