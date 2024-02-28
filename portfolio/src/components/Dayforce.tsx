8bf143cf6be5aa61265c70a2a5fdfdfbfb497e21
nopunindented <umerfiaz251@gmail.com>
2024-02-28 16:56:49 -0700

    "Very good styling set"

--- portfolio/src/components/Dayforce.tsx
+++ portfolio/src/components/Dayforce.tsx
@@ -5,7 +5,12 @@ const Dayforce: React.FC = () => {
     <>
       <div className="dayforce-container">
         <div className="dayforce-name">Dayforce (formerly Ceridian)</div>
-        <div className="dayforce-line" />
+        <div className="dayforce-position">Software Engineer Intern</div>
+        <ul className="dayforce-points">
+            <li>Will provide operational support for software delivery management tools such as Azure DevOps,
+Azure, Jira, Artifactory, Visual Studio/Code, GitHub, etc</li>
+            <li>Tech stack consists of: C#, .NET, Docker, Azure DevOps, Github Actions, Azure, MSSQL</li>
+        </ul>
       </div>
     </>
   );
