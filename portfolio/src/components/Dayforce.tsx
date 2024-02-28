7b4cccba530810c0e20cdc99fb20dfe443542838
nopunindented <umerfiaz251@gmail.com>
2024-02-28 16:09:47 -0700

    "Will likely shift to full line for ease"

---
+++ portfolio/src/components/Dayforce.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Dayforce: React.FC = () => {
+  return (
+    <>
+      <div className="dayforce-container">
+        <div className="dayforce-name">Dayforce (formerly Ceridian)</div>
+        <div className="dayforce-line" />
+      </div>
+    </>
+  );
+};
+
+export default Dayforce;
--- portfolio/src/pages/SecondPage.tsx
+++ portfolio/src/pages/SecondPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Dayforce from "../components/Dayforce";
 
 const SecondPage: React.FC = () => {
   return (
     <>
       <div className="second-page">
         <div className="work-header">Work Experience</div>
+        <Dayforce />
       </div>
     </>
   );
