16b9867d7e801c85d55df000fcb6bbf8a862699d
nopunindented <umerfiaz251@gmail.com>
2024-02-28 17:19:34 -0700

    "Base created for Dayforce work experience"

--- portfolio/src/components/Dayforce.tsx
+++ portfolio/src/components/Dayforce.tsx
@@ -5,11 +5,13 @@ const Dayforce: React.FC = () => {
     <>
       <div className="dayforce-container">
         <div className="dayforce-name">Dayforce (formerly Ceridian)</div>
+        <div className="dayforce-date">Incoming @May 2024</div>
         <div className="dayforce-position">Software Engineer Intern</div>
         <ul className="dayforce-points">
+        <li>Intern on the Developer Experience (Internal Software Tool Dev & Infrastructure/DevOps) team.</li>
             <li>Will provide operational support for software delivery management tools such as Azure DevOps,
-Azure, Jira, Artifactory, Visual Studio/Code, GitHub, etc</li>
-            <li>Tech stack consists of: C#, .NET, Docker, Azure DevOps, Github Actions, Azure, MSSQL</li>
+Azure, Jira, Artifactory, Visual Studio/Code, GitHub, Confluence, etc.</li>
+            <li>Tech stack consists of: C#, .NET, Docker, Azure DevOps, Github Actions, Azure, MSSQL, and many more DevOps tools.</li>
         </ul>
       </div>
     </>
