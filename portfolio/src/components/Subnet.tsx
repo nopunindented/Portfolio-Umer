a50a0f54b77c78f7927f186d83cccf42721a9524
nopunindented <umerfiaz251@gmail.com>
2024-02-29 23:49:42 -0700

    "Decent columning and rowing for subnet container"

--- portfolio/src/components/Subnet.tsx
+++ portfolio/src/components/Subnet.tsx
@@ -4,6 +4,9 @@ const Subnet: React.FC = () => {
   return (
     <>
       <div className="subnet-container">
+        <div className="subnet-name">SUBNET Solutions</div>
+        <div className="subnet-date">May 2024 - Present</div>
+        <div className="subnet-position">Software Developer Intern</div>
       </div>
     </>
   );
