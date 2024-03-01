1341290983a512e4cdd179b7c6dd0f21bdd9f2bd
nopunindented <umerfiaz251@gmail.com>
2024-03-01 13:18:39 -0700

    "re-usable set up improvement"

--- portfolio/src/components/University.tsx
+++ portfolio/src/components/University.tsx
@@ -4,10 +4,10 @@ const University: React.FC = () => {
   return (
     <>
       <div className="uni-container">
-        <div className="subnet-name">University of Alberta</div>
+        <div className="dayforce-name">University of Alberta</div>
         <div className="subnet-date">May 2023 - August 2023</div>
-        <div className="subnet-position">Fullstack Developer</div>
-        <ul className="uni-points">
+        <div className="dayforce-position">Fullstack Developer</div>
+        <ul className="dayforce-points">
         <li> Developed a payments page containing different pricing plans using TypeScript, React, Next.js, and Figma.</li>
             <li> Reduced errors users faced by 41.8% by enhancing UI designs, and improving and creating UX designs in Figma.</li>
             <li> Created application implementing the API 1183 practice for assessing pipelines utilizing a Docker-containerized
