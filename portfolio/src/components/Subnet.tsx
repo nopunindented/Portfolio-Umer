1f629328db1026d1dc697dab23d4cee2b68cd07e
nopunindented <umerfiaz251@gmail.com>
2024-03-01 13:06:28 -0700

    "Point shifting and centralization"

--- portfolio/src/components/Subnet.tsx
+++ portfolio/src/components/Subnet.tsx
@@ -4,10 +4,10 @@ const Subnet: React.FC = () => {
   return (
     <>
       <div className="subnet-container">
-        <div className="subnet-name">SUBNET Solutions</div>
-        <div className="subnet-date">May 2024 - Present</div>
-        <div className="subnet-position">Software Developer Intern</div>
-        <ul className="subnet-points">
+        <div className="dayforce-name">SUBNET Solutions</div>
+        <div className="dayforce-date">May 2024 - Present</div>
+        <div className="dayforce-position">Software Developer Intern</div>
+        <ul className="dayforce-points">
         <li>Devising Python AI microservice for querying data on 4 million sensory devices for 300+ companies, using Langchain and Django.</li>
             <li>Creating Typescript-based React interface for Django and .NET REST API pipeline hosted on Azure VM.</li>
             <li>Reduced API calls in TypeScript React UI by over 80% via batching them through new TypeScript Node endpoints.</li>
