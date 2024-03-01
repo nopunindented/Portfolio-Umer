986377abfcb794069d82c136d63152cb2b1512c4
nopunindented <umerfiaz251@gmail.com>
2024-03-01 00:47:29 -0700

    "All that's left is PainWorth"

--- portfolio/src/components/University.tsx
+++ portfolio/src/components/University.tsx
@@ -6,7 +6,13 @@ const University: React.FC = () => {
       <div className="uni-container">
         <div className="subnet-name">University of Alberta</div>
         <div className="subnet-date">May 2023 - August 2023</div>
-        <div className="subnet-position">Software Developer Intern</div>
+        <div className="subnet-position">Fullstack Developer</div>
+        <ul className="uni-points">
+        <li> Developed a payments page containing different pricing plans using TypeScript, React, Next.js, and Figma.</li>
+            <li> Reduced errors users faced by 41.8% by enhancing UI designs, and improving and creating UX designs in Figma.</li>
+            <li> Created application implementing the API 1183 practice for assessing pipelines utilizing a Docker-containerized
+Flask REST API Python workflow that uses Pandas, NumPy, and Seaborn for data visualization.</li>
+        </ul>
       </div>
     </>
   );
