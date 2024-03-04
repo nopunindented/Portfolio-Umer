93534d0f476d2c2e2e55aeb8b93ee3ad0853aa7c
nopunindented <umerfiaz251@gmail.com>
2024-03-03 23:27:57 -0700

    "Will create horizontal scroll bar"

---
+++ portfolio/src/components/ScrollBar.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import './../index.css';
+
+const prodList = [
+    {
+      id: 1,
+      name: "Prod 1",
+      description: "lorem ipsum lorem lorems ndjkfnfvjkndfjkvn"
+    },
+    {
+      id: 2,
+      name: "Prod 2",
+      description: "lorem ipsum lorem lorems ndjkfnfvjkndfjkvn"
+    },
+    {
+      id: 3,
+      name: "Prod 3",
+      description: "lorem ipsum lorem lorems ndjkfnfvjkndfjkvn"
+    },
+    {
+      id: 4,
+      name: "Prod 4",
+      description: "lorem ipsum lorem lorems ndjkfnfvjkndfjkvn"
+    },
+    {
+      id: 5,
+      name: "Prod 5",
+      description: "lorem ipsum lorem lorems ndjkfnfvjkndfjkvn"
+    },
+    {
+      id: 6,
+      name: "Prod 6",
+      description: "lorem ipsum lorem lorems ndjkfnfvjkndfjkvn"
+    },
+    {
+      id: 7,
+      name: "Prod 7",
+      description: "lorem ipsum lorem lorems ndjkfnfvjkndfjkvn"
+    },
+    {
+      id: 8,
+      name: "Prod 8",
+      description: "lorem ipsum lorem lorems ndjkfnfvjkndfjkvn"
+    },
+    {
+      id: 9,
+      name: "Prod 9",
+      description: "lorem ipsum lorem lorems ndjkfnfvjkndfjkvn"
+    }
+  ];
+
+
+
+const ScrollBar: React.FC = () => {
+  return (
+    <>
+      <div className="third-page">
+
+      </div>
+    </>
+  );
+};
+
+export default ScrollBar;
--- portfolio/src/pages/ThirdPage.tsx
+++ portfolio/src/pages/ThirdPage.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './../index.css';
+import ScrollBar from '../components/ScrollBar';
 
 const ThirdPage: React.FC = () => {
   return (
     <>
       <div className="third-page">
-
+        <ScrollBar />
       </div>
     </>
   );
