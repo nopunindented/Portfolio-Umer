17d4c705d82861cefcf587ef581314526b506fac
nopunindented <umerfiaz251@gmail.com>
2024-03-04 23:50:39 -0700

    "Will leave in map format for now but will change it, tomorrow"

--- portfolio/src/components/ScrollBar/ScrollBar.tsx
+++ portfolio/src/components/ScrollBar/ScrollBar.tsx
@@ -21,18 +21,14 @@ const ScrollBar: React.FC = () => {
   return (
     <div className="scroll-container">
       <div
-        ref={containerRef}
-        style={{
-          overflowX: 'scroll',
-          scrollBehavior: 'smooth',
-          whiteSpace: 'nowrap', // Ensure children are in a horizontal line
-        }}
       >
-        {/* Add your components here */}
-      </div>
-      <div className="action-btns">
-        <button onClick={() => handleScroll(-200)}>Scroll Left</button>
-        <button onClick={() => handleScroll(200)}>Scroll Right</button>
+        <div className="content-box">
+          {ProjectData.map((item) => (
+            <div className="card" style={{ backgroundColor: item.color }}>
+              <p>{item.id}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
