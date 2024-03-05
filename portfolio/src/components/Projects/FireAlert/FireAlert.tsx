dd35de410ad7f65bd26d94bccef3d676999e910e
nopunindented <umerfiaz251@gmail.com>
2024-03-05 14:17:42 -0700

    "Applied extending on hover effect to top row"

--- portfolio/src/components/Projects/FireAlert/FireAlert.tsx
+++ portfolio/src/components/Projects/FireAlert/FireAlert.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import './../projects.css';
 
 const FireAlert: React.FC = () => {
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+
+  const handleHover = () => {
+    setIsHovered(true);
+   };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
     <>
-      <div className="firealert-container">
+      <div className={`firealert-container ${isHovered ? 'hovered' : ''}`} onMouseEnter={handleHover} onMouseLeave={handleMouseLeave}>
       </div>
     </>
   );
--- portfolio/src/components/Projects/VoltScript/VoltScript.tsx
+++ portfolio/src/components/Projects/VoltScript/VoltScript.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import './../projects.css';
 
 const VoltScript: React.FC = () => {
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+
+  const handleHover = () => {
+    setIsHovered(true);
+   };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
   return (
     <>
-      <div className="voltscript-container">
+      <div className={`voltscript-container ${isHovered ? 'hovered' : ''}`} onMouseEnter={handleHover} onMouseLeave={handleMouseLeave}>
         
       </div>
     </>
