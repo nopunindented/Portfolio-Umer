0c6d96ec1f4d6e0d4dc5a7d512d9ff301a7e422c
nopunindented <umerfiaz251@gmail.com>
2024-03-04 23:45:19 -0700

    "Instead of mapping, will look at creating separate components and implementing them inside"

--- portfolio/src/components/ScrollBar/ProjectData.tsx
+++ portfolio/src/components/ScrollBar/ProjectData.tsx
@@ -3,22 +3,13 @@ import React from 'react';
 interface Data {
   id: string;
   color: string;
+  projectName: string;
+  description: string
+
 }
 
 const ProjectData: Data[] = [
-  { id: "01", color: "#014f5e" },
-  { id: "02", color: "#015a6b" },
-  { id: "03", color: "#016679" },
-  { id: "04", color: "#017186" },
-  { id: "05", color: "#1a7f92" },
-  { id: "06", color: "#348d9e" },
-  { id: "07", color: "#4d9caa" },
-  { id: "08", color: "#67aab6" },
-  { id: "09", color: "#80b8c3" },
-  { id: "06", color: "#348d9e" },
-  { id: "07", color: "#4d9caa" },
-  { id: "08", color: "#67aab6" },
-  { id: "09", color: "#80b8c3" },
+  { id: "01", color: "#014f5e", projectName: "ThriftAid", description: 'hello'},
 ];
 
 export default ProjectData;
