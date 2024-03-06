856f867b1db75bdfb2186a8055c3c8a1b9a0ccba
nopunindented <umerfiaz251@gmail.com>
2024-03-05 23:01:39 -0700

    "alignment match"

--- portfolio/src/components/Projects/FireAlert/FireAlert.tsx
+++ portfolio/src/components/Projects/FireAlert/FireAlert.tsx
@@ -16,7 +16,7 @@ const FireAlert: React.FC = () => {
     <>
       <div className={`firealert-container ${isHovered ? 'hovered' : ''}`} onMouseEnter={handleHover} onMouseLeave={handleMouseLeave}>
         <div className="firealertrow-two" >
-          <div className="firealert-logo" style={{gridColumn: "2", gridRow: "1", fontFamily: "Oswald", fontSize:"300%", color: "#FF8928", height: "80%", position: "relative"}}>
+          <div className="firealert-logo" style={{gridColumn: "2", gridRow: "1", fontFamily: "Oswald", fontSize:"300%", color: "#FF8928", position: "relative"}}>
             Fire Alert!
           </div>
           <div className="thriftaid-name" style={{gridColumn: "2", gridRow: "2", fontFamily: "Space Grotesk, sans-serif", position: "relative", fontSize: "200%", fontWeight: "600", color: "rgb(216, 191, 191)"}}>
