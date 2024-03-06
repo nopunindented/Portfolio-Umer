4a7894c1dd365a28ebc19b47b8ff8f0bb86f7bdb
nopunindented <umerfiaz251@gmail.com>
2024-03-05 22:44:52 -0700

    "Need to fix gridding for images"

--- portfolio/src/components/Projects/FireAlert/FireAlert.tsx
+++ portfolio/src/components/Projects/FireAlert/FireAlert.tsx
@@ -15,8 +15,13 @@ const FireAlert: React.FC = () => {
   return (
     <>
       <div className={`firealert-container ${isHovered ? 'hovered' : ''}`} onMouseEnter={handleHover} onMouseLeave={handleMouseLeave}>
-        <div className="firealert-logo" style={{gridColumn: "2", gridRow: "2", fontFamily: "Oswald", fontSize:"300%", color: "#FF8928"}}>
-          Fire Alert!
+        <div className="firealertrow-two" >
+          <div className="firealert-logo" style={{gridColumn: "2", gridRow: "1", fontFamily: "Oswald", fontSize:"300%", color: "#FF8928", height: "80%", position: "relative"}}>
+            Fire Alert!
+          </div>
+          <div className="thriftaid-name" style={{gridColumn: "2", gridRow: "2", fontFamily: "Space Grotesk, sans-serif", position: "relative", fontSize: "200%", fontWeight: "600", color: "rgb(216, 191, 191)"}}>
+            ThriftAid
+          </div>
         </div>
       </div>
     </>
--- portfolio/src/components/Projects/ThriftAid/ThriftAid.tsx
+++ portfolio/src/components/Projects/ThriftAid/ThriftAid.tsx
@@ -16,9 +16,11 @@ const ThriftAid: React.FC = () => {
   return (
     <>
       <div className={`thriftaid-container ${isHovered ? 'hovered' : ''}`} onMouseEnter={handleHover} onMouseLeave={handleMouseLeave}>
-        <img src={logo} alt="thriftaid logo" style={{gridColumn: "2", gridRow: "2"}}/>
-        <div className="thriftaid-name" style={{gridColumn: "2", gridRow: "3", fontFamily: "Space Grotesk, sans-serif", position: "relative", top: "-45%", marginTop: "10%", fontSize: "200%", fontWeight: "600", color: "rgb(216, 191, 191)"}}>
+        <div className="thriftaidrow-two">
+          <img src={logo} alt="thriftaid logo" style={{gridColumn: "2", gridRow: "1", position: "relative"}}/>
+          <div className="thriftaid-name" style={{gridColumn: "2", gridRow: "2", fontFamily: "Space Grotesk, sans-serif", position: "relative", fontSize: "200%", fontWeight: "600", color: "rgb(216, 191, 191)"}}>
           ThriftAid
+        </div>
         </div>
       </div>
     </>
