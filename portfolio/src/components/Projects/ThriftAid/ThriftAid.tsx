295a9173bca46b76935b1b44c05bed66657fe995
nopunindented <umerfiaz251@gmail.com>
2024-03-06 09:00:09 -0700

    "Good styling for thriftaid descript text"

--- portfolio/src/components/Projects/ThriftAid/ThriftAid.tsx
+++ portfolio/src/components/Projects/ThriftAid/ThriftAid.tsx
@@ -17,10 +17,15 @@ const ThriftAid: React.FC = () => {
     <>
       <div className={`thriftaid-container ${isHovered ? 'hovered' : ''}`} onMouseEnter={handleHover} onMouseLeave={handleMouseLeave}>
         <div className="thriftaidrow-two">
-          <img src={logo} alt="thriftaid logo" style={{gridColumn: "2", gridRow: "1", position: "relative"}}/>
-          <div className="thriftaid-name" style={{gridColumn: "2", gridRow: "2", fontFamily: "Space Grotesk, sans-serif", position: "relative", fontSize: "200%", fontWeight: "600", color: "rgb(216, 191, 191)"}}>
-          ThriftAid
+          <img src={logo} alt="thriftaid logo" style={{gridRow: "1", position: "relative"}}/>
+          <div className="thriftaid-name" style={{gridRow: "2", fontFamily: "Space Grotesk, sans-serif", position: "relative", fontSize: "200%", fontWeight: "600", color: "rgb(216, 191, 191)"}}>
+            ThriftAid
+          </div>
         </div>
+        <div className="thriftaidrow-three">
+          <div className="thriftaid-name" style={{gridRow: "1", fontFamily: "Fira Code", position: "relative", fontSize: "100%", fontWeight: "600", color: "white"}}>
+          ThriftAid is a platform that links homeless shelters with thrift. Thrift stores can donate excess stock by creating postings which homeless shelters may choose to accept.
+          </div>
         </div>
       </div>
     </>
