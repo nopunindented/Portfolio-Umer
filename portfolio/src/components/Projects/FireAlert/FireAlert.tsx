264242a5a02fd93eee075d594a43e4fec95cac03
nopunindented <umerfiaz251@gmail.com>
2024-03-07 20:17:50 -0700

    "Fire alert good to go"

--- portfolio/src/components/Projects/FireAlert/FireAlert.tsx
+++ portfolio/src/components/Projects/FireAlert/FireAlert.tsx
@@ -14,32 +14,32 @@ const FireAlert: React.FC = () => {
   };
 
   const skills = [
-    'Scikit-Learn',
-    'Twilio',
     'Python',
-    'React Native',
+    'Twilio',
+    'Scikit-Learn',
+    'GCP',
     'Flask',
-    'GCP'
+    'React Native'
     ,'JavaScript'
   ];
 
   return (
     <>
       <div className={`firealert-container ${isHovered ? 'hovered' : ''}`} onMouseEnter={handleHover} onMouseLeave={handleMouseLeave}>
-        <div className="firealertrow-two" >
-          <div className="firealert-logo" style={{gridColumn: "1", gridRow: "1", fontFamily: "Oswald", fontSize:"300%", color: "#FF8928", position: "relative"}}>
+        <div className="thriftaidrow-two" >
+          <div className="thriftaid-logo" style={{gridRow: "1", fontFamily: "Oswald", fontSize:"300%", color: "#FF8928", position: "relative"}}>
             Fire Alert!
           </div>
-          <div className="firealert-name" style={{gridColumn: "1", gridRow: "2", fontFamily: "Space Grotesk, sans-serif", position: "relative", fontSize: "200%", fontWeight: "600", color: "rgb(216, 191, 191)"}}>
+          <div className="thriftaid-name" style={{gridRow: "2", fontFamily: "Space Grotesk, sans-serif", position: "relative", fontSize: "200%", fontWeight: "600", color: "rgb(216, 191, 191)"}}>
             Fire Alert
           </div>
         </div>
-        <div className="firealertrow-three">
-          <div className="firealert-name" style={{gridRow: "1", fontFamily: "Fira Code", position: "relative", fontSize: "81%", fontWeight: "600", color: "white"}}>
-          Fire Alert is an android app that provided real-time fire alerts to workers, such as farmers, in remote areas. The user selects a location and then the app provides SMS notifications if a fire is within a certain distance of the specified location.
+        <div className="thriftaidrow-three">
+          <div className="thriftaid-name" style={{gridRow: "1", fontFamily: "Fira Code", position: "relative", fontSize: "90%", fontWeight: "600", color: "white"}}>
+            ThriftAid is a platform that links homeless shelters with thrift stores. Thrift stores can donate excess stock by creating postings which homeless shelters may choose to accept.
           </div>
           <a href="https://github.com/nopunindented/ThriftAid">
-            <img src={link} alt="link" style={{gridRow: "2", position: "relative", width: "42%", marginTop: "15%"}}/>
+            <img src={link} alt="link" style={{gridRow: "2", position: "relative", width: "42%", marginTop: "20%"}}/>
           </a>
         </div>
         <div className="firealertrow-four">
