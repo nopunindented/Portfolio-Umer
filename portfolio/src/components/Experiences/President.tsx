f29eb212c875e29e6a264b2c7e6bba2b446e146c
nopunindented <umerfiaz251@gmail.com>
2024-03-10 23:03:31 -0600

    "Descript"

--- portfolio/src/components/Experiences/President.tsx
+++ portfolio/src/components/Experiences/President.tsx
@@ -10,10 +10,10 @@ const President: React.FC = () => {
         <div className="president-club">UofA Computer Engineering Club</div>
         <ul className="president-points">
             <li>Co-running for President in the 2024 CompE Club election.</li>
-            <li>Plan to greatly increase financial transparency - especially on HackED - and get rid of blatant fund misusage across the club.</li>
+            <li>Plan to greatly increase financial transparency across the club, especially in HackED & Geer Week.</li>
             <li>Will replace most current junior exec roles by giving each portfolio 2-4 "officer" roles that are interview-based, in order to ensure greater committment.</li>
             <li>Aiming for complete club financial self-sufficiency through greatly increasing number of sponsors at our major events - Aether & HackED.</li>
-            <li>HackED has had negligible oversight for the past 2 years. We plan to shift that by integrating it fully into the club, just like Aether is.</li>
+            <li>Planning to increase oversight across HackED and fully integrating it into the club by adding clauses in the constitution requiring full transparency and approval for all financial decisions.</li>
             <li>I will continue working on inter-university partnerships as well - such as an expansion of our current one with UBC's CS club (which I cultivated).</li>
         </ul>
       </div>
