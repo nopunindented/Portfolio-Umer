1774b80693e3cdef54b4c7467d1e4138d5a9f5b7
nopunindented <umerfiaz251@gmail.com>
2024-03-12 00:30:31 -0600

    "Navbar complete"

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/Home";
-import Header from "./components/Header";
 import GridHeader from "./components/GridHeader";
 
 function App() {
   return (
     <>
-    <GridHeader />
       <div className="App">
         <HomePage />
+        <GridHeader />
       </div>
+      
     </>
   );
 }
