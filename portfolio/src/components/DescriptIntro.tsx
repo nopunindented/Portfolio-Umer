80761251742be0bff7e19d2cf15b9579375c77d2
nopunindented <umerfiaz251@gmail.com>
2024-03-12 00:13:34 -0600

    "Just about done first page edits"

--- portfolio/src/components/DescriptIntro.tsx
+++ portfolio/src/components/DescriptIntro.tsx
@@ -7,8 +7,7 @@ const DescriptIntro: React.FC = () => {
         <div>
         I'm a 3rd year undergraduate software engineering student at the University of Alberta, a Software Developer Intern at
         SUBNET Solutions, and an incoming Software Engineer Intern at Dayforce, on the Developer Experience Team (Infrastructure/DevOps & Internal Software Tool Dev).
-        
-        I enjoy creating applications that tackle social problems (or are just cool) and I love being part of university clubs!:)
+        My work revolves around creating software applications that tackle social problems & improve one's daily life, as well as helping student communities.
         </div>
       </div>
     </>
