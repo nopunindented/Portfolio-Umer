ee4137303c23a4379ce87edfa66314c249a299ef
nopunindented <umerfiaz251@gmail.com>
2024-03-12 17:09:01 -0600

    "404 page finished"

--- portfolio/src/pages/NoPage.tsx
+++ portfolio/src/pages/NoPage.tsx
@@ -4,7 +4,14 @@ const NoPage: React.FC = () => {
     return (
       <>
         <div className="nopage">
-
+            <div className="nopage-exist">
+                <div className="nopage-text">
+                    Page Not Found :(
+                </div>
+                <div className="nopage-text-secondary">
+                    Looks like the page you're looking for doesn't exist. Click <a className="fourofoura" href="/">here</a> to return to the home page.
+                </div>
+            </div>
         </div>
       </>
     );
