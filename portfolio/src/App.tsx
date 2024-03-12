4f15d31dcc8e214c2438bbfb0faefcc2481e31ac
nopunindented <umerfiaz251@gmail.com>
2024-03-12 16:41:56 -0600

    "404 configuration set"

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import GridHeader from "./components/GridHeader";
 import SlideAnimation from "./components/SlideAnimation";
 import SecondPage from "./pages/SecondPage";
 import ThirdPage from "./pages/ThirdPage";
 import FourthPage from "./pages/FourthPage";
+import NoPage from "./pages/NoPage";
 
 function App() {
-  return (
-    <>
-      <div className="App">
-        <SlideAnimation />
-        <SecondPage />
-        <ThirdPage />
-        <FourthPage />
-        <GridHeader />
-      </div>
-      
-    </>
-  );
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  const handlePathChange = () => {
+    setCurrentPath(window.location.pathname);
+  };
+
+  window.addEventListener("popstate", handlePathChange);
+
+  const renderPage = () => {
+    switch (currentPath) {
+      case "/":
+      case "/#workexperience":
+      case "/#projects":
+      case "/#extracurriculars":
+      case "/#home":
+        return (
+          <>
+            <SlideAnimation />
+            <SecondPage />
+            <ThirdPage />
+            <FourthPage />
+            <GridHeader />
+          </>
+        );
+      default:
+        return <NoPage />;
+    }
+  };
+
+  return <div className="App">{renderPage()}</div>;
 }
 
 export default App;
---
+++ portfolio/src/pages/NoPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NoPage: React.FC = () => {
+    return (
+      <>
+        <div id="proje" className="thirage">
+
+          </div>
+      </>
+    );
+  };
+  
+  export default NoPage;
